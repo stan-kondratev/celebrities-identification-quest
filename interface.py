f166d5df83c808c13f029df4549124b7252a5ac9
joaquim albuquerque <jalbuquerque@sapo.pt>
2023-06-19 11:25:16 +0100

    Updated score description

--- interface.py
+++ interface.py
@@ -2,11 +2,12 @@
 import pandas as pd
 import streamlit as st
 from logic import DataRetrieve
-import datetime
-import numpy as np
-import os
+# import datetime
+# import numpy as np
+# import os
 import unicodedata
 
+st.set_page_config(layout="centered")
 '''
 # Celebrities Guessing with Images
 '''
@@ -20,9 +21,11 @@ def strip_accents(s):
                   if unicodedata.category(c) != 'Mn')
 
 df = get_data_celebrities()
+score_words = ["Frigid","Bitterly Cold","Chilling","Brisk","Refreshing","Pleasantly Mild","Balmy","Sweltering","Roasting","Scorching Hot"]
+
 
 if 'score_list' not in st.session_state:
-    st.session_state['score_list'] = [(0,0)]
+    st.session_state['score_list'] = [(0,0,0)]
 
 hidden_celebrity, hint = DataRetrieve.celeb_selector(file_path="raw_data/metafile.csv")
 
@@ -31,7 +34,7 @@ df.sort_values(by="name",axis=0, ascending=True, inplace=True)
 values =df['name'].tolist()
 keys = df.index.to_list()
 
-col1, col2 = st.columns(2,gap="large")
+col1, col2 = st.columns([0.4,0.6],gap="small")
 
 with col1:
     """
@@ -49,10 +52,9 @@ with col1:
             if hidden_celebrity==celebrity:
                 score = (a,100)
                 st.balloons()
-
             else:
                 score = DataRetrieve.celeb_and_score_query(hidden_celebrity=hidden_celebrity,celebrity=celebrity,names_df=pd.read_csv("raw_data/names.csv"),score_df=pd.read_csv("raw_data/scoring.csv"))
-            st.session_state["score_list"].append([a,score[1]])
+            st.session_state["score_list"].append([a,score[1],score_words[int(score[1]/10)]])
 
     with col1_2:
         if st.button("Hint", use_container_width=True):
@@ -67,5 +69,5 @@ with col2:
     """
     st.text(" ")
     st.text(" ")
-    df_score = pd.DataFrame(st.session_state["score_list"], columns=["Name","Score"])
-    st.dataframe(df_score.iloc[1:].sort_values(by="Score", ascending=False),use_container_width=True,column_config={0:"Attempt",1:"Name",2:"Score"})
+    df_score = pd.DataFrame(st.session_state["score_list"], columns=["Name","Score","Score description"])
+    st.dataframe(df_score.iloc[1:].sort_values(by="Score", ascending=False),use_container_width=True,column_config={0:"Seq",1:"Name                       ",2:"Score",3:""})
